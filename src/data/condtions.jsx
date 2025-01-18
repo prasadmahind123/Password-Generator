5dd1b2a1c0928d882012b5dafc02a1a604941db5
prasadmahind123 <163630263+prasadmahind123@users.noreply.github.com>
2025-01-19 00:17:28 +0530

    Add files via upload

---
+++ src/data/condtions.jsx
@@ -0,0 +1,4 @@
+export let upperCaseOption = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export let lowerCaseOption = 'abcdefghijklmnopqrstuvwxyz';
+export let numbersOption = '0123456789';
+export let symbolOptions = '!@#$%^&*+-{}[]/.<>()'
